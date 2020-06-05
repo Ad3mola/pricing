ab9627091d9ebedf6d289dec53bd3b33e5622397
Ademola Madelewi <ademolamadelewi@gmail.com>
2020-06-05 01:13:52 +0100

    done

--- src/components/PricingPage.js
+++ src/components/PricingPage.js
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import PriceCard from "./PriceCard";
-import { toast } from "react-toastify";
 
 export default function PricingPage() {
-  const [loading, setLoading] = useState(false);
-
   const plans = [
     {
       id: "bronze",
@@ -42,23 +39,16 @@ export default function PricingPage() {
     },
   ];
 
-  const choosePlan = (arg) => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      toast.success(`${arg} plan subscription successful`, {
-        toastId: "success",
-      });
-    }, 3000);
-  };
-
   return (
     <div className="mb-5 container">
       <div className="text-center mt-3">
         <h3>Pick a pricing plan</h3>
         <p>Subscribe to one of our new pricing plans</p>
       </div>
-      <PriceCard plans={plans} choosePlan={choosePlan} loading={loading} />
+      <div className="row">
+        {plans.length &&
+          plans.map((plan) => <PriceCard render={plan} key={plan} />)}
+      </div>
     </div>
   );
 }
